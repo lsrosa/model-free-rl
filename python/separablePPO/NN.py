d93c1e2b835b6c82a78eb280549352d3328221df
leandro <le.souza.rosa@gmail.com>
2024-02-13 18:19:07 +0100

    test tensor with grad

---
+++ python/separablePPO/NN.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+import itertools as it
+import numpy as np
+
+# Torch imports
+from tensordict.nn import TensorDictModule
+from tensordict.nn.distributions import NormalParamExtractor
+from torchrl.modules import ProbabilisticActor, TanhNormal, ValueOperator
+
+class NN(torch.nn.Module):
+    
+    def __init__(self, env):
+        super(NN, self).__init__()
+        self.env = env
+
+    def define_actor_network(self, dimentions):
+        self.actor_dimentions = dimentions
+        self.actor = nn.Sequential()
+ 
+        for n_in, n_out in zip(dimentions[:-2], dimentions[1:-1]):
+            self.actor.append(nn.Linear(n_in, n_out))
+            self.actor.append(nn.Tanh())
+        self.actor.append(nn.Linear(dimentions[-2], dimentions[-1])) #output
+        self.actor.append(NormalParamExtractor())
+        
+        _policy_module = TensorDictModule(
+            self.actor, in_keys=["observation"], out_keys=["loc", "scale"]
+        )
+
+        self.policy_module  = ProbabilisticActor(
+                module = _policy_module,
+                spec = self.env.action_spec,
+                in_keys = ["loc", "scale"],
+                distribution_class = TanhNormal,
+                distribution_kwargs = {
+                    "min": self.env.action_spec.space.low,
+                    "max": self.env.action_spec.space.high,
+                },
+                return_log_prob=True,
+                )
+        return
+
+    def define_value_network(self, dimentions):
+        self.value_dimentions = dimentions
+        self.value_net = nn.Sequential()
+
+        for n_in, n_out in zip(dimentions[:-2], dimentions[1:-1]):
+            self.value_net.append(nn.Linear(n_in, n_out))
+            self.value_net.append(nn.Tanh())
+        self.value_net.append(nn.Linear(dimentions[-2], dimentions[-1]))
+        
+        self.value_module = ValueOperator(
+            module=self.value_net,
+            in_keys=["observation"],
+        )
+        return
+
+    def forward(self, x):
+        return self.actor(x)
---
+++ python/separablePPO/test.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import itertools as it
+import numpy as np
+from matplotlib import pyplot as plt
+
+class NN(torch.nn.Module):
+    
+    def __init__(self):
+        self.nin = 2
+        self.n1 = 5
+        self.nglue = 4
+        self.n2 = 10
+        self.nout = 1
+
+        super(NN, self).__init__()
+        self.nn1 = nn.Sequential()
+        self.nn1.append(nn.Linear(self.nin, self.n1))
+        self.nn1.append(nn.ReLU())
+        self.nn1.append(nn.Linear(self.n1, self.n1))
+        self.nn1.append(nn.ReLU())
+        self.nn1.append(nn.Linear(self.n1, self.n1))
+        self.nn1.append(nn.ReLU())
+        self.nn1.append(nn.Linear(self.n1, self.nglue))
+        
+        for l in self.nn1.children():
+            if isinstance(l, nn.Linear):
+                torch.nn.init.xavier_uniform_(l.weight)
+                l.bias.data.fill_(0.0)
+
+        self.nn2 = nn.Sequential()
+        self.nn2.append(nn.Linear(self.nglue, self.n2))
+        self.nn1.append(nn.ReLU())
+        self.nn2.append(nn.Linear(self.n2, self.n2))
+        self.nn1.append(nn.ReLU())
+        self.nn2.append(nn.Linear(self.n2, self.n2))
+        self.nn1.append(nn.ReLU())
+        self.nn2.append(nn.Linear(self.n2, self.nout))
+        
+        for l in self.nn2.children():
+            if isinstance(l, nn.Linear):
+                torch.nn.init.xavier_uniform_(l.weight)
+                l.bias.data.fill_(0.0)
+
+    def forward(self, x):
+        _x = self.nn1(x).detach().numpy()
+        self.glue = torch.tensor(_x, requires_grad = True)
+        _y = self.nn2(self.glue)
+        return _y
+
+def f(x):
+    r = (x[:,0]**2 + x[:,1]**2)
+    return r.reshape(len(r), 1)
+
+if __name__ == "__main__":
+    nn = NN()
+    optim = torch.optim.SGD(nn.parameters(), lr=1e-5)
+    loss_f = torch.nn.MSELoss(reduction='sum')
+    d_size = 500
+    for i in range(100):
+        optim.zero_grad()
+        
+        x = torch.rand(d_size, 2)
+        y = f(x)
+        y_pred = nn(x)
+        loss = loss_f(y, y_pred)
+        print(loss)
+        loss.sum().backward()
+        optim.step()
+
+    # test
+    d_size = 100
+    x = torch.rand(d_size, 2)
+    y = f(x) 
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    with torch.no_grad():
+        y_pred = nn(x).detach().numpy()
+    #print(y, y_pred)
+    ax.scatter(x[:,0], x[:,1], y, alpha = 0.5, label='label')
+    ax.scatter(x[:,0], x[:,1], y_pred, alpha=0.5, label='pred')
+    plt.legend()
+    plt.show()
+
+
