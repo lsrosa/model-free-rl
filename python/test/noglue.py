3f5c23b2bd2cd6a1c3d9fe3492258b504703cade
leandro <le.souza.rosa@gmail.com>
2024-02-14 16:16:45 +0100

    add test without glue loss

---
+++ python/test/noglue.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import itertools as it
+from matplotlib import pyplot as plt
+import os
+
+from NN import *
+
+if __name__ == "__main__":
+    nn = NN()
+    
+    # loss function and optmizer
+    #optim = torch.optim.SGD(nn.parameters()), lr=1e-4)
+    optim = torch.optim.SGD(nn.parameters(), lr=1e-4)
+    
+    loss_f = torch.nn.MSELoss(reduction='sum')
+    #loss_f_glue = torch.nn.MSELoss(reduction='sum')
+
+    d_size = 500
+    for i in range(5000):
+        
+        # labels
+        x = torch.rand(d_size, 2)
+        
+        # train glue
+        #print('!!!!!!before')
+        #print_params(nn) 
+        
+        #y_glue = f_glue(x)
+        #optim.zero_grad()
+        #y_pred_glue = nn.forward_glue(x)
+        #loss_glue = loss_f_glue(y_glue, y_pred_glue)
+         
+        #print('!!!!!!after1')
+        #print_params(nn) 
+
+        #freeze(nn.nn1, True)
+        y = f(x)
+        optim.zero_grad()
+        y_pred = nn.forward_all(x)
+
+        loss = loss_f(y, y_pred)# + loss_glue
+        loss.backward()
+        optim.step()
+
+        #print('!!!!!!after2')
+        #print_params(nn) 
+        print('loss: ', loss.data)
+    # test
+    d_size = 100
+    x = torch.rand(d_size, 2)
+    y = f(x) 
+    y_glue = f_glue(x)
+
+    with torch.no_grad():
+        y_pred_glue = nn.nn1(x).detach().numpy()
+        y_pred = nn(x).detach().numpy()
+
+    fig = plt.figure()
+
+    #print(y, y_pred)
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+    ax.scatter(x[:,0], x[:,1], y, alpha = 0.5, label='label')
+    ax.scatter(x[:,0], x[:,1], y_pred, alpha=0.5, label='pred')
+    plt.legend()
+
+    ax = fig.add_subplot(1, 2, 2)
+    ax.scatter(x[:,0], y_glue, alpha=0.5, label='label')
+    ax.scatter(x[:,0], y_pred_glue, alpha=0.5, label='pred')
+    plt.legend()
+    plots_folder = 'plots/'
+    if not os.path.exists(plots_folder):
+        os.makedirs(plots_folder)
+    plt.savefig('plots/noglue.png')
+    plt.show()
+
+
